568f588643e795d86e0d960f814f96d2f3fa9475
ASHOK MAHARJAN <ashok997@gmail.com>
2019-07-24 23:39:01 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,8 +17,7 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT COUNT(species)
-  FROM Characters
-  "
+  FROM Characters";
 end
 
 def select_name_and_series_subgenres_of_authors
