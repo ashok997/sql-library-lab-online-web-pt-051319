f49db5c45c6ae5502e6fa6dfe1c3c7da139b2a25
ASHOK MAHARJAN <ashok997@gmail.com>
2019-07-24 23:56:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT Authors.name,Series.subgenre "
+  "SELECT Authors.name,Subgenres.name 
+  FROM Authors
+  INNER JOIN Series
+  On Author.id = Series.author_id ";
 end
 
 def select_series_title_with_most_human_characters
