04fbb5ce7f0dac297bcf34cd39988bab7ffcc57b
ASHOK MAHARJAN <ashok997@gmail.com>
2019-07-24 23:37:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,22 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT Books.title, Books.year
+  FROM Books
+  INNER JOIN Series
+  ON Books.series_id = Series.id
+  WHERE Series.id = 1
+  ORDER BY Books.year";
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name,motto 
+  FROM Characters
+  ORDER BY motto
+  Limit 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species"
 end
 
 def select_name_and_series_subgenres_of_authors
