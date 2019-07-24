07ba2e5536f4b8851e6e940238c85d9b47899007
ASHOK MAHARJAN <ashok997@gmail.com>
2019-07-24 23:43:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,11 +18,11 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species)
   FROM Characters
-  ORDER BY species";
+  ORDER BY COUNT(species)";
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT "
 end
 
 def select_series_title_with_most_human_characters
