a0a9781704f1d7543b6d105e0e020ee97ad5c1ef
ASHOK MAHARJAN <ashok997@gmail.com>
2019-07-25 02:40:14 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT Series.title, COUNT (Character.series_id)
+  "SELECT Series.title, COUNT (Characters.series_id)
   FROM Characters
   INNER JOIN Series ON Characters.series_id = Series.id
   WHERE Characters.species = 'human'";
