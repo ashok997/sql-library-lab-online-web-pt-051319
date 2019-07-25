b971265e26c42303250879418c1faeb5f8ca8b37
ASHOK MAHARJAN <ashok997@gmail.com>
2019-07-25 01:36:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT Authors.name, Subgenres.name 
   FROM Authors
   INNER JOIN Series ON Authors.id = Series.author_id
-  INNER JOIN Subgenres ON Series.sub";
+  INNER JOIN Subgenres ON Series.subgenre_id = Subgenres.id";
 end
 
 def select_series_title_with_most_human_characters
