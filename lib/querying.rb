dcf5689ef2bb200e962a696ac098c97e605ecd63
ASHOK MAHARJAN <ashok997@gmail.com>
2019-07-25 02:11:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT Characters.name, Count(books)
+  "SELECT Characters.name, Count(books.id)
   FROM Characters
   INNER JOIN Character_books
   GROUP BY Characters.name";
